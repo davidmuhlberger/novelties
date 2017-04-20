15203ce7576e0644c5a7aad2989727c74ed50f1f
davidmuhlberger <david.muhlberger@gmail.com>
2017-04-20 20:39:30 +0200

    add crud products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,26 @@ class ProductsController < ApplicationController
 
 
   def index
-      @products = Product.all
+    @products = Product.all
   end
 
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,23 @@ Rails.application.routes.draw do
 
   get "/team" => "pages#team"
   get "/contact" => "pages#join_us"
-  get "/products" => "products#index"
-  get "/products/:id" => "products#show"
+
+  resources :products, only: [:new, :create, :index, :show]
+
+  # Read action routes
+  #get "/products" => "products#index"
+  #get "/products/:id" => "products#show"
+
+  #Create action routes
+  #get "/products/new" => "products#new"
+  #post "/products" => "products#create"
+
+  #Update action routes
+  #get "/products/:id/edit" => "products#edit"
+  #patch "/products/:id" => "products#update"
+
+  #Delete action routes
+  #delete "/products/:id" => "products#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
