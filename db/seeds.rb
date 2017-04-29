17f4ba9797c2134a41f563d77e87ccded628836d
davidmuhlberger <david.muhlberger@gmail.com>
2017-04-29 21:21:30 +0200

    upvotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,10 @@ User.destroy_all
 
 david = User.create(email: 'david.muhlberger@gmail.com', password: 'testtest')
 
-Product.create!(user: david, name: "Kuodz", url: "http://getkudoz.com", tagline:"Tinder for job searchs", category: "tech")
+kudoz = Product.create!(user: david, name: "Kuodz", url: "http://getkudoz.com", tagline:"Tinder for job searchs", category: "tech")
 Product.create!(user: david, name: "uSlide", url: "http://youslide.io", tagline:"Youtube sucks for education", category: "education")
 Product.create!(user: david, name: "Medpics", url: "http://medpics.io", tagline:"Share your diagnosis", category: "tech")
-Product.create!(user: david, name: "Le Wagon", url: "http://lewagon.org", tagline:"Best coding bootcamp", category: "education")
+lewagon = Product.create!(user: david, name: "Le Wagon", url: "http://lewagon.org", tagline:"Best coding bootcamp", category: "education")
+
+kudoz.upvotes.create! user: david
+lewagon.upvotes.create! user: david
