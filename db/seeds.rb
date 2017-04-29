72e363b68ca38191cf812aab195928ed669cbf09
davidmuhlberger <david.muhlberger@gmail.com>
2017-04-29 16:53:21 +0200

    devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
+User.destroy_all
 
-Product.create!(name: "Kuodz", url: "http://getkudoz.com", tagline:"Tinder for job searchs")
-Product.create!(name: "uSlide", url: "http://youslide.io", tagline:"Youtube sucks for education")
-Product.create!(name: "Medpics", url: "http://medpics.io", tagline:"Share your diagnosis")
+david = User.create(email: 'david.muhlberger@gmail.com', password: 'testtest')
+
+Product.create!(user: david, name: "Kuodz", url: "http://getkudoz.com", tagline:"Tinder for job searchs", category: "tech")
+Product.create!(user: david, name: "uSlide", url: "http://youslide.io", tagline:"Youtube sucks for education", category: "education")
+Product.create!(user: david, name: "Medpics", url: "http://medpics.io", tagline:"Share your diagnosis", category: "tech")
+Product.create!(user: david, name: "Le Wagon", url: "http://lewagon.org", tagline:"Best coding bootcamp", category: "education")
